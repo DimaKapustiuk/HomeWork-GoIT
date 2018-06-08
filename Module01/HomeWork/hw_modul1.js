f440e9ebaf280a9169565fb6260b0a5c2fd50c50
Дмитрий Капустюк <dima.kapustiuk@gmail.com>
2018-06-08 22:08:10 +0300

    Add function changeTour

--- Module01/HomeWork/hw_modul1.js
+++ Module01/HomeWork/hw_modul1.js
@@ -20,6 +20,31 @@
 const ADMIN_LOGIN = 'admin';
 const ADMIN_PASSWORD = 'm4ngo1zh4ackz0r';*/
 
+/*
+  ⚠️ ДОПОЛНИТЕЛЬНОЕ ЗАДАНИЕ - ВЫПОЛНЯТЬ ПО ЖЕЛАНИЮ
+  
+  Создайте скрипт турагенства, продающего поездки в 3-х группах: sharm, hurgada и taba.
+  Кол-во мест в группах ограничено (создайте переменные для хранения мест в группах): 
+    * sharm - 15
+    * hurgada - 25
+    * taba - 6.
+  Когда пользователь посещает страницу, ему необходимо предложить ввести число необходимых мест,
+  результат сохранить в переменную.
+  Необходимо проверить являются ли введенные данные целым положительным числом. 
+  
+    - В случае неверного ввода от пользователя, скрипт показывает alert с текстом 
+      "Ошибка ввода" и больше ничего не делает.
+    - В случае верного ввода, последовательно проверить кол-во мест в группах, 
+      и кол-во необходимых мест введенных пользователем.
+  Если была найдена группа в которой количество мест больше либо равно необходимому, 
+  вывести сообщение через confirm, что есть место в группе такой-то, согласен ли 
+  пользоваетель быть в этой группе?
+    * Если ответ да, показать alert с текстом 'Приятного путешествия в группе <имя группы>'
+    * Если ответ нет, показать alert с текстом 'Нам очень жаль, приходите еще!'
+  
+  Если мест нигде нет, показать alert с сообщением 'Извините, столько мест нет ни в одной группе!'
+*/
+
 let login = prompt("Введите логин!");
 const CANCEL = "Отменено пользователем";
 const ACCESS_OUT = "Доступ запрещен!";
@@ -34,10 +59,55 @@ if (login === null) {
   if (passwd === null) {
     alert(CANCEL);
   } else if (passwd === ADMIN_PASSWORD) {
-    alert(ACCESS_IN)
+    alert(ACCESS_IN);
+    changeTour();
   } else {
     alert(ACCESS_OUT);
   }
 } else {
   alert(ACCESS_OUT);
+}
+
+function changeTour() {
+  let userData = prompt("Введите целое число необходимых мест для Вашей групы!");
+  console.log(userData);
+  let userGroup = Number(userData);
+  console.log(userGroup);
+  const SHARM = 15;
+  const HURGADA = 25;
+  const TABA = 6;
+  const SHARM_GROUP = "Sharm!";
+  const HURGADA_GROUP = "Hurgada!";
+  const TABA_GROUP = "Taba!";
+  let sumPlaces = SHARM + TABA + HURGADA;
+
+  if (userGroup % 1 === 0 && userGroup != 0) {
+    alert("Да верно, проверяем к-во в групах");
+    if (userGroup <= TABA) {
+      const CHOISE_SHARM = confirm(`Есть свободное место в групе ${TABA_GROUP}, вас устроит?`);
+      if (CHOISE_SHARM === true) {
+        alert(`Приятного путешествия в группе ${TABA_GROUP}`);
+      } else {
+        alert("Нам очень жаль приходите еще!")
+      }
+    } else if (userGroup > TABA && userGroup <= SHARM) {
+      const CHOISE_SHARM = confirm(`Есть свободное место в групе ${SHARM_GROUP}, вас устроит?`);
+      if (CHOISE_SHARM === true) {
+        alert(`Приятного путешествия в группе ${SHARM_GROUP}`);
+      } else {
+        alert("Нам очень жаль приходите еще!")
+      }
+    } else if (userGroup > SHARM && userGroup <= HURGADA) {
+      const CHOISE_SHARM = confirm(`Есть свободное место в групе ${HURGADA_GROUP}, вас устроит?`);
+      if (CHOISE_SHARM === true) {
+        alert(`Приятного путешествия в группе ${HURGADA_GROUP}`);
+      } else {
+        alert("Нам очень жаль приходите еще!")
+      }
+    } else if (userGroup >= sumPlaces) {
+      alert("Извените мест больше нет!");
+    }
+  } else {
+    alert("Ошибка ввода!");
+  }
 }
