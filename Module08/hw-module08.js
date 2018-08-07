8611008a254ae5987a94994f043922bfde921435
Дмитрий Капустюк <dima.kapustiuk@gmail.com>
2018-08-07 15:35:24 +0300

    maybe a finish work

--- Module08/hw-module08.js
+++ Module08/hw-module08.js
@@ -9,7 +9,7 @@
       <!-- li будет столько, сколько объектов в массиве картинок. Эти 3 для примера -->
       <ul class="preview">
         <li><img src="img/preview-1.jpeg" data-fullview="img/fullview-1.jpeg" alt="alt text 1"></li>
-        <li><img src="img/preview-2.jpeg" data-fullview="img/fullview-2.jpeg" alt="alt text 2"></li>
+        <li><img src="img/preview-2.jpeg" data-fullview="https://placeimg.com/1000/400/any" alt="alt text 2"></li>
         <li><img src="img/preview-3.jpeg" data-fullview="img/fullview-3.jpeg" alt="alt text 3"></li>
       </ul>
     </div>   
@@ -53,7 +53,7 @@
 */
 
 const galleryItems = [
-  { preview: 'img/preview-1.jpeg', fullview: 'img/fullview-1.jpeg', alt: "alt text 1" },
+  { preview: 'img/preview-1.jpeg', fullview: 'img/fullwiew-1.jpg', alt: "alt text 1" },
   { preview: 'img/preview-2.jpeg', fullview: 'img/fullview-2.jpeg', alt: "alt text 2" },
   { preview: 'img/preview-3.jpeg', fullview: 'img/fullview-3.jpeg', alt: "alt text 3" },
   { preview: 'img/preview-4.jpeg', fullview: 'img/fullview-4.jpeg', alt: "alt text 4" },
@@ -62,25 +62,97 @@ const galleryItems = [
 ];
 
 const gallery = document.querySelector('.js-image-gallery');
-const fullview = document.createElement('div');
-const list = document.createElement('ul');
+const createFullView = () => document.createElement('div');
+const createPreview = () => document.createElement('ul');
+const createLi = () => document.createElement('li');
+const createImg = () => document.createElement('img');
+
+const createImageFullview = ({fullview, alt}) => {
+  const fullviewDiv = createFullView();
+  const imageFullview = createImg();
+
+  imageFullview.src = fullview;
+  imageFullview.alt = alt;
+  fullviewDiv.append(imageFullview);
+
+  return fullviewDiv;
+}
+
+const createImagePreview = ({ preview, fullview, alt }) => {
+  const imagePreview = createImg();
+
+  imagePreview.src = preview;
+  imagePreview.alt = alt;
+  imagePreview.dataset.fullview = fullview;
+
+  return imagePreview;
+}
+
+const createPreviewItem = obj => {
+  const itemPreview = createLi();
+  const imagePreview = createImagePreview(obj);
+
+  itemPreview.classList.add('image-wrapper')
+  itemPreview.append(imagePreview);
+
+  return itemPreview;
+};
+
+const addPreviwItem = images => {
+  const addImages = images.map(image => createPreviewItem(image));
+
+  return addImages;
+};
+
+const hoverDiv = () => {
+  const imageWrapper = document.querySelectorAll('.image-wrapper');
+
+  const randColor = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    const color = "#" + r.toString(16) + g.toString(16) + b.toString(16);
+
+    return color;
+  };
+
+  imageWrapper.forEach(val => {
+
+    val.onmouseover = () => {
+      val.style.border = `10px solid ${randColor()}`;
+    };
+
+    val.onmouseout = () => {
+      val.style.border = '10px solid #fff';
+    };
+  });
+};
 
 
- const createPreviemItem = ({preview, fullview, alt}) => {
-     
+const onLoad = () => {
+  const fullview = createImageFullview(galleryItems[0]);
+  const list = createPreview();
+  const images = addPreviwItem(galleryItems);
+  const fullImage = fullview.querySelector('img');
 
- }
-const createFullViem = () => {
-    fullview.classList.add('fullview');
-    list.classList.add('preview');
-    gallery.append(fullview, list);
- }
+  const onClickImage = event => {
+    const dataSrc = event.target.dataset.fullview;
+    const imgAlt =  event.target.alt;
+
+    fullImage.src = dataSrc;
+    fullImage.alt = imgAlt;
+};
+
+  fullview.classList.add('fullview');
+  list.classList.add('preview');
+  list.append(...images);
+  gallery.append(fullview, list);
+
+  list.addEventListener('click', onClickImage);
+  hoverDiv();
+};
+
+document.addEventListener('DOMContentLoaded', onLoad);
 
 
-
-window.onload = createFullViem;
-
-const ul = document.querySelector('.preview')
-
-console.log(ul)
 
